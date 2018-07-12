ad99b9feb6684d2ec962a4174e13d0606ce9c992
Utkarsh Srivastav <usrivastav615@gmail.com>
2018-07-12 06:07:40 +0530

    Adding test cases and code refractor

--- sites/tests.py
+++ sites/tests.py
@@ -1,6 +1,56 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from datetime import datetime
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+from sites.models import Site
+
+class SiteViewsBaseTests(TestCase):
+
+    def setUp(self):
+        site = Site.allSites.create(name='ABC')
+        site.sitevalues_set.create(aValue=1.1, bValue=2.6)
+        site.sitevalues_set.create(aValue=2.4, bValue=2.9)
+        site.sitevalues_set.create(aValue=4.5, bValue=10.6)
+
+class SiteSummationViewsTests(SiteViewsBaseTests):
+
+    def test_summation(self):
+        response = self.client.get(reverse('getSumOfSiteValues'))
+        site = response.context['sitesSummary'][0]
+        self.assertEqual(site.name, 'ABC')
+        self.assertAlmostEqual(site.aggregateAValue, 8.0, places=2)
+        self.assertAlmostEqual(site.aggregateBValue, 16.1, places=2)
+
+    def test_average(self):
+        response = self.client.get(reverse('getAverageOfSiteValues'))
+        site = response.context['sitesSummary'][0]
+        self.assertEqual(site.name, 'ABC')
+        self.assertAlmostEqual(site.aggregateAValue, 2.666, places=2)
+        self.assertAlmostEqual(site.aggregateBValue, 5.366, places=2)
+
+class SiteAverageViewsTests(SiteViewsBaseTests):
+
+    def test_average(self):
+        response = self.client.get(reverse('getAverageOfSiteValues'))
+        site = response.context['sitesSummary'][0]
+        self.assertEqual(site.name, 'ABC')
+        self.assertAlmostEqual(site.aggregateAValue, 2.666, places=2)
+        self.assertAlmostEqual(site.aggregateBValue, 5.366, places=2)
+
+class SiteDetailViewTests(SiteViewsBaseTests):
+
+    def compareSiteValues(self, siteValue, aValue, bValue):
+        self.assertEqual(siteValue.aValue, aValue)
+        self.assertEqual(siteValue.bValue, bValue)
+
+    def test_summation(self):
+        response = self.client.get(reverse('siteDetail', kwargs={'pk':1}))
+        siteValues = response.context['siteValues']
+        self.compareSiteValues(siteValues[0], 1.1, 2.6)
+        self.compareSiteValues(siteValues[1], 2.4, 2.9)
+        self.compareSiteValues(siteValues[2], 4.5, 10.6)
+
+
--- sites/views/SitesAverage.py
+++ sites/views/SitesAverage.py
@@ -3,16 +3,20 @@ from base import *
 class SitesAverage(ListView):
     template_name = 'SiteSummary.html'
 
+    RAW_QUERY_AVERAGE_OF_SITE_VALUES = '''select sites_site.id, name, avg("aValue") as "aggregateAValue", 
+                                        avg("bValue") as "aggregateBValue" from sites_site left outer join 
+                                        sites_sitevalues on sites_site.id = sites_sitevalues.site_id 
+                                        group by sites_site.id order by sites_site.id;'''
+
     def get(self, request, *args, **kwargs):
         return TemplateResponse(request, self.template_name,
                                 self.get_context_data())
 
     def get_context_data(self):
 
-        sqlQuery = 'select sites_site.id, name, avg("aValue") as "aggregateAValue", avg("bValue") as "aggregateBValue" from sites_site left outer join sites_sitevalues on sites_site.id = sites_sitevalues.site_id group by sites_site.id order by sites_site.id;'
         sites = []
         try:
-            sites = Site.allSites.raw(sqlQuery)
+            sites = Site.allSites.raw(self.RAW_QUERY_AVERAGE_OF_SITE_VALUES)
         except Exception as e:
             pass
 
